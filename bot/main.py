77f1e1d8bfeeabf1c334d2a40d7c8f823c400c6c
Izzath Nisfer <izzathnisfer@gmail.com>
2026-01-03 21:07:45 +0530

    Debug: Use relative workdir

--- bot/main.py
+++ bot/main.py
@@ -19,7 +19,7 @@ app = Client(
     api_id=config.API_ID,
     api_hash=config.API_HASH,
     bot_token=config.BOT_TOKEN,
-    workdir=str(config.DATA_DIR)
+    workdir='./data'
 )
 
 @app.on_message(filters.command("ping"))
