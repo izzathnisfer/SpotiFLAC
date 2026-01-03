5901a39b779a7680b1077b74b801b2862ab7c342
Izzath Nisfer <izzathnisfer@gmail.com>
2026-01-03 20:31:58 +0530

    Fix: Revert to legacy loop handling

--- bot/main.py
+++ bot/main.py
@@ -51,7 +51,8 @@ async def main():
 
 if __name__ == "__main__":
     try:
-        asyncio.run(main())
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(main())
     except KeyboardInterrupt:
         pass
     except Exception as e:
